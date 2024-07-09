87a5cd774e3c261a55d2b7c93833ce45ecf8e81d
mc <MochenFly@gmail.com>
2024-07-09 23:11:37 +0800

    feat: 添加进度条进度控制

--- Src/MCVideoPlayTest/MCVideoPlayTest.cpp
+++ Src/MCVideoPlayTest/MCVideoPlayTest.cpp
@@ -10,6 +10,8 @@ MCVideoPlayTest::MCVideoPlayTest(QWidget* parent)
     m_pVideoWidget = new MCWidget::MCVideoWidget(ui->wgtVideo);
     ui->hLayoutVideo->addWidget(m_pVideoWidget);
 
+    ui->sliderTime->setValueByCliucked(true);
+
     m_pVideoPlayer = new MCWidget::MCVideoPlayer(this);
     connect(ui->btnPlay, &QPushButton::clicked, this, [&]()
     {
@@ -20,6 +22,26 @@ MCVideoPlayTest::MCVideoPlayTest(QWidget* parent)
     connect(m_pVideoPlayer, &MCWidget::MCVideoPlayer::sigFrameChanged, this, [&](std::shared_ptr<MCWidget::MCVideoFrame> frame)
     {
         m_pVideoWidget->updateFrame(frame);
+    }, Qt::BlockingQueuedConnection);
+
+    connect(m_pVideoPlayer, &MCWidget::MCVideoPlayer::sigTimeChanged, this, [&](qint64 time)
+    {
+        ui->sliderTime->blockSignals(true);
+        ui->sliderTime->setValue(time);
+        ui->sliderTime->blockSignals(false);
+    });
+
+    connect(ui->sliderTime, &QSlider::valueChanged, this, [&](int value)
+    {
+        m_pVideoPlayer->seek(value);
+    });
+
+    connect(m_pVideoPlayer, &MCWidget::MCVideoPlayer::sigStateChanged, this, [&](MCWidget::MCVideoPlayer::VideoState state)
+    {
+        if (MCWidget::MCVideoPlayer::VideoState::PlayingState == state)
+        {
+            m_pVideoPlayer->startTimer();
+        }
     }, Qt::BlockingQueuedConnection);
 }
 
