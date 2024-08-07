12953b1a35da337af576efdb27d06876a10110d4
mc <MochenFly@gmail.com>
2024-08-07 23:51:52 +0800

    feat: 修改 MCVideoFrame 实现，分别管理 y u v 数据

--- Src/MCWidget/Video/MCVideoFrame.cpp
+++ Src/MCWidget/Video/MCVideoFrame.cpp
@@ -10,20 +10,48 @@ MCVideoFrame::~MCVideoFrame()
     freeData();
 }
 
-void MCVideoFrame::setYUVData(const uint8_t* data, int width, int height)
+void MCVideoFrame::setYUVData(const uint8_t* data, int width, int height, const uint8_t* y, const uint8_t* u, const uint8_t* v)
 {
     freeData();
 
     m_width = width;
     m_height = height;
+
     int dataSize = width * height * 3 / 2;
     m_YUVData = (uint8_t*)malloc(dataSize);
     memcpy(m_YUVData, data, dataSize);
+
+    int ySize = width * height;
+    int uvSize = (width / 2) * (height / 2);
+
+    m_YData = (uint8_t*)malloc(ySize);
+    memcpy(m_YData, y, ySize);
+
+    m_UData = (uint8_t*)malloc(uvSize);
+    memcpy(m_UData, u, uvSize);
+
+    m_VData = (uint8_t*)malloc(uvSize);
+    memcpy(m_VData, v, uvSize);
 }
 
 uint8_t* MCVideoFrame::getYUVData()
 {
     return m_YUVData;
+}
+
+uint8_t* MCVideoFrame::getYData()
+{
+    return m_YData;
+}
+
+uint8_t* MCVideoFrame::getUData()
+{
+    return m_UData;
+}
+
+uint8_t* MCVideoFrame::getVData()
+{
+    return m_VData;
 }
 
 int MCVideoFrame::getWidth()
@@ -43,4 +71,22 @@ void MCVideoFrame::freeData()
         free(m_YUVData);
         m_YUVData = nullptr;
     }
+
+    //if (nullptr != m_YData)
+    //{
+    //    free(m_YData);
+    //    m_YData = nullptr;
+    //}
+
+    //if (nullptr != m_UData)
+    //{
+    //    free(m_UData);
+    //    m_UData = nullptr;
+    //}
+
+    //if (nullptr != m_VData)
+    //{
+    //    free(m_VData);
+    //    m_VData = nullptr;
+    //}
 }
